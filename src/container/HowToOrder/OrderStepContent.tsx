f65b48242bdc3a6586e109d8d8b246d0f53554ae
Ahmad N. F <fahmadnaufal@gmail.com>
2019-05-09 19:57:12 +0700

    add howto

--- src/container/HowToOrder/OrderStepContent.tsx
+++ src/container/HowToOrder/OrderStepContent.tsx
@@ -9,11 +9,24 @@ class OrderStepContent extends Component<{}, {}> {
           <div style={{ backgroundColor: '#333', height: '450px' }} />
         </Col>
         <Col xs='12'>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus feugiat placerat felis, non imperdiet enim convallis at. Vivamus ac mi dolor. Aliquam erat volutpat. Phasellus rutrum accumsan sapien sed sodales. Praesent ornare purus elit, eu iaculis risus gravida at. Curabitur rutrum maximus tortor eget lacinia. Sed metus sapien, accumsan eu tellus non, cursus malesuada tellus. Curabitur tellus risus, scelerisque eu fringilla nec, accumsan eu diam.
-            <br /><br />Vestibulum volutpat eleifend eros. Morbi mattis malesuada sem, vitae mattis massa rhoncus sit amet. Phasellus faucibus, lacus eu commodo gravida, tortor massa faucibus enim, et accumsan ex tortor quis augue. Donec euismod volutpat risus. Duis blandit vehicula gravida. Morbi ac purus nec odio dapibus porta. Maecenas elementum vel dolor sed convallis. Sed ante augue, tristique eu scelerisque sed, fringilla non felis. Sed ultrices scelerisque lorem. Cras feugiat fermentum arcu, id porta justo lacinia ut.
-            <br /><br />Nunc tempus tincidunt nibh, eu congue dolor porta vitae. Mauris lacinia eu libero in venenatis. Quisque quis efficitur ex. Phasellus ligula orci, tincidunt nec neque eu, ornare bibendum nisi. Quisque pulvinar nibh placerat aliquam aliquet. Curabitur mi nunc, posuere dapibus ante id, interdum dapibus diam. Morbi rhoncus ligula in turpis accumsan rutrum. Pellentesque id enim id ligula tempus rutrum ut a ex. Quisque eget venenatis sapien. Morbi non blandit nulla. Etiam nulla dolor, dictum vel vestibulum id, tempus non magna. Etiam tempor nisl interdum est iaculis, eu viverra nisi venenatis. Vivamus euismod viverra quam, at cursus mi imperdiet nec.
-          </p>
+          <ol>
+            <li>
+              Choose Item<br/>
+              Click on Shop Tab and visit product detail page for an item you wish to buy. Tap on Quantity of the item you wish to buy, and tap Buy.
+            </li><br/>
+            <li>
+              View Cart<br/>
+              You will be redirected to you cart. Your item(s) will be displayed here, along with total price. Tap Continue Shopping if you wish to add another item to your cart. Or tap checkout if you already done your shop.
+            </li><br/>
+            <li>
+              Checkout<br/>
+              Review your order information and fill the the address of delivery with the preferred delivery method. If you have any coupon code, please tap Use Coupon Code and fill the code to get the benefit. Tap Pay to proceed to payment.
+            </li><br/>
+            <li>
+              Pay<br/>
+              Pay your ordered products and trips through our third-party payment gateway, Midtrans. Carefully follow the instructions listed on the Midtrans payment page.
+            </li>
+          </ol>
         </Col>
       </Row>
     )
