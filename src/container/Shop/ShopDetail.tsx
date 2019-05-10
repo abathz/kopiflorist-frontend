622b7e69d785272aba8c099a16c3db7394360fff
Ahmad N. F <fahmadnaufal@gmail.com>
2019-05-10 08:44:15 +0700

    fix layout in shop detail

--- src/container/Shop/ShopDetail.tsx
+++ src/container/Shop/ShopDetail.tsx
@@ -115,7 +115,26 @@ class ShopDetail extends Component<PropsComponent, StateComponent> {
             <p className='text-m text-black text-hel-reg'>{product.detail}</p>
             <div className='mb-4' style={{ borderBottom: '1pt solid #979797' }} />
             <Row>
-              <Col xs='4'>
+              <Col xs='12' className='pt-1 pb-2'>
+                <div>
+                  <span
+                    className={`text-black ${product.discount > 0 ? 'text-hel-reg text-m' : 'text-hel-95 text-ml'}`}
+                    style={{ textDecoration: product.discount > 0 ? 'line-through' : 'none' }}
+                  >
+                    IDR {product.price}
+                  </span>
+                  <span className='text-s text-black' style={{ textDecoration: product.discount > 0 ? 'line-through' : 'none' }}> / {product.weight} {product.weight_in}</span>
+                </div>
+                {product.discount > 0 &&
+                  <div>
+                    <span className='text-hel-95 text-ml' style={{ color: 'red' }}>IDR {product.discount.price_discount}</span>
+                    <span className='text-s' style={{ color: 'red' }}> / {product.weight} {product.weight_in}</span>
+                  </div>
+                }
+              </Col>
+            </Row>
+            <Row>
+              <Col xs='6'>
                 <FormGroup>
                   <Input type='select' id='quantity' onChange={this.onInputChange}>
                     <option defaultChecked={true}>Quantity</option>
@@ -126,23 +145,6 @@ class ShopDetail extends Component<PropsComponent, StateComponent> {
                     <option value='5'>5</option>
                   </Input>
                 </FormGroup>
-              </Col>
-              <Col xs='4' className='pt-2'>
-                <div>
-                  <span
-                    className={`text-black ${product.discount > 0 ? 'text-hel-reg text-m' : 'text-hel-95 text-ml'}`}
-                    style={{ textDecoration: product.discount > 0 ? 'line-through' : 'none' }}
-                  >
-                    IDR {product.price}
-                  </span>
-                  <span className='text-s text-black' style={{ textDecoration: product.discount > 0 ? 'line-through' : 'none' }}> / {product.weight_in}</span>
-                </div>
-                {product.discount > 0 &&
-                  <div>
-                    <span className='text-hel-95 text-ml' style={{ color: 'red' }}>IDR {product.discount.price_discount}</span>
-                    <span className='text-s' style={{ color: 'red' }}> / {product.weight_in}</span>
-                  </div>
-                }
               </Col>
               <Col xs='4' className='pt-2'>
                 <span className='text-ml text-black'>Stock: <span className='text-black text-hel-95 text-l'>{product.quantity}</span></span>
